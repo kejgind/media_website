8ad956bb0f8e0a6c6567bf6387a32a7c61a3a1bc
Krzysztof Gie <kejgind@gmail.com>
2018-01-10 16:34:54 +0100

    setting dev workspace for wordpress build

---
+++ wp-build/footer.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Media_2_WordPress
+ */
+
+?>
+
+	<footer id="main-footer" class="text-center p-4">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<p>Copyright 2017 &copy; MEDIA</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+	<!-- VIDEO MODAL -->
+	<div class="modal fade" id="videoModal">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-body">
+					<button class="close" data-dismiss="modal">
+						<span>&times;</span>
+					</button>
+					<iframe src="" height="450" width="100%" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="wp-content/themes/media2wp/assets/js/jquery.min.js"></script>
+	<script src="wp-content/themes/media2wp/assets/js/popper.min.js"></script>
+	<script src="wp-content/themes/media2wp/assets/js/bootstrap.min.js"></script>
+	<script src="wp-content/themes/media2wp/assets/js/ekko-lightbox.min.js"></script>
+	<script src="wp-content/themes/media2wp/assets/js/scripts.js"></script>
+
+	</body>
+
+	</html>
